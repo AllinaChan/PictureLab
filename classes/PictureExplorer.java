f14cc25396782aa901996c5c4a537abcbb1bb918
BT_1N3_29 <zchen5143@bths.edu>
2019-03-15 14:24:39 -0400

    BIG LEVIN

--- classes/PictureExplorer.java
+++ classes/PictureExplorer.java
@@ -800,8 +800,9 @@ public class PictureExplorer implements MouseMotionListener, ActionListener, Mou
    */
   public static void main( String args[])
   {
-    Picture pix = new Picture("beach.jpg");
-    pix.explore();
+    Picture pix = new Picture("download.jpg");
+    Picture bigLevin = pix.scale(2,2);
+    bigLevin.explore();
   }
   
 }
