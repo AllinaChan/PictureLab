4962405e024747d3b2caca50a14697294bc6aa1f
BT_1N3_30 <d>
2019-03-18 14:35:49 -0400

    largest

--- classes/IntArrayWorker.java
+++ classes/IntArrayWorker.java
@@ -124,5 +124,21 @@ public class IntArrayWorker
       }
     }
   }
+
+  public int getLargest()
+  {
+    int largest = 0;
+    for (int row = 0; row < matrix.length; row++)
+    {
+      for (int col = 0; col < matrix[0].length; col++)
+      {
+
+        if (matrix[row][col]> largest){
+          largest = matrix[row][col];
+        }
+
+      }
+    } return largest;
+  }
  
 }
