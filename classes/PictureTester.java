5be5541676d8658e0312a93b0b7082b8abacdf6c
BT_1N3_30 <d>
2019-03-22 14:28:42 -0400

    seagull

--- classes/PictureTester.java
+++ classes/PictureTester.java
@@ -115,7 +115,11 @@ public class PictureTester
     gull.explore();
   }
 
-
+  public static void mirrorGull(){
+    Picture seagull=new Picture("seagull.jpg");
+    seagull.mirrorHorizontal();
+    seagull.explore();
+  }
   /** Main method for testing.  Every class can have a main
     * method in Java */
   public static void main(String[] args)
